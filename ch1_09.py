b63994969e83cb0a3c74dbaeface4cedf67107e1
ia-lsinclair <32363944+ia-lsinclair@users.noreply.github.com>
2017-11-13 11:39:28 -0500

    9

--- ch1_09.py
+++ ch1_09.py
@@ -1,3 +1,10 @@
 # Name
 # Problem #
 # Chapter 1 Problem Set Part 1
+number_str = input("Enter your number")
+number_int = int(number_str)
+number_int = number_int + 2
+number_int = number_int * 3
+number_int = number_int - 6
+number_int =  number_int / 3
+print(number_int)
