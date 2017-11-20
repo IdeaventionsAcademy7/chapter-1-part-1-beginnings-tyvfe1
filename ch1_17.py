7c55bb30a3220ba8c4e9d5713bc015564776297c
ia-lsinclair <32363944+ia-lsinclair@users.noreply.github.com>
2017-11-20 11:36:39 -0500

    17

--- ch1_17.py
+++ ch1_17.py
@@ -1,3 +1,9 @@
 # Name
 # Problem #
 # Chapter 1 Problem Set Part 1
+something_string = input("put in something")
+print (something_string)
+something_int = int (something_string)
+print (something_int)
+something_float = float (something_string)
+print (something_float)
--- delete.py
+++
@@ -1,2 +0,0 @@
-my_ver1 = 7.0
-my_ver2 = print(my_ver1 % my_ver2)
